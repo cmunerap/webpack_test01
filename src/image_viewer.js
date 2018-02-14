ccc5fdb5101664f542c734719e3516f117ea9e0c
Cristian Múnera <cmunerap@gmail.com>
2018-02-13 23:58:38 -0500

    Se agregan archivos faltantes de configuración de babel-loader y image_viewer

---
+++ src/image_viewer.js
@@ -0,0 +1,8 @@
+import '../styles/image_viewer.css';
+
+const image = document.createElement('img');
+image.src = 'http://lorempixel.com/400/400'
+//image.src = 'https://picsum.photos/400/400'
+//image.src = 'http://pipsum.com/400x400.jpg'
+
+document.body.appendChild(image);
